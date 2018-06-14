860f4b5dbab8898763c7b5110d52ccd2596a63c1
Voumo Tiogo Joel Brice <tiogoj@hotmail.com>
2018-06-14 18:11:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var recipes = {
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({},object,{[key]: value});
 }
-
+// update the object with the key and the value
  function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
    object[key]=value;
    return object;
